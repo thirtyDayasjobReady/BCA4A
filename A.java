2ec8a0f47689a017bfc869dda23298c0ea267150
Vivek <vivek_kumar@liet.in>
2026-04-02 11:57:39 +0530

    Updated B.java

--- A.java
+++ A.java
@@ -1,6 +1,6 @@
 public class A {
     public static void main(String[] args) {
-        System.out.println("Hi this is first program............");
+        System.out.println("Hi this is first program..............");
 
     }
 }
--- B.java
+++ B.java
@@ -1,5 +1,6 @@
 public class B {
     public static void main(String[] args) {
         System.out.println("Hello....................");
+        System.out.println("Hello..This is my first program..................");
     }
 }
