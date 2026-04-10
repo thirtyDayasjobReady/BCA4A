b95519365c96aafd60f623174f9dbb3cf15db60c
Vivek <vivek_kumar@liet.in>
2026-04-10 14:21:15 +0530

    Harish Ji Library, Roop Vinayak Food Order

---
+++ M1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+public class M1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int[][] A = new int[2][2];
+        int[][] B = new int[2][2];
+        int[][] C = new int[2][2];
+        System.out.print("Enter Element Of First Matrix:- ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                A[i][j] = s.nextInt();
+            }
+        }
+        System.out.print("Enter Element Of Second Matrix:- ");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                B[i][j] = s.nextInt();
+            }
+        }
+        //-------Print Matrix A-----------------
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+            {
+                C[i][j]=A[i][j]+B[i][j];
+                //System.out.print(A[i][j] +"+"+ B[i][j]+"="+C[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++)
+            {
+                System.out.print(C[i][j]+"\t");
+            }
+            System.out.println("");
+        }
+    }
+}
---
+++ MatrixAddition.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class MatrixAddition {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] A = new int[2][2];
+        int[][] B = new int[2][2];
+        int[][] sum = new int[2][2];
+
+        // Input first matrix
+        System.out.println("Enter elements of first 2x2 matrix:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                A[i][j] = sc.nextInt();
+            }
+        }
+
+        // Input second matrix
+        System.out.println("Enter elements of second 2x2 matrix:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                B[i][j] = sc.nextInt();
+            }
+        }
+
+        // Addition
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                sum[i][j] = A[i][j] + B[i][j];
+            }
+        }
+
+        // Output result
+        System.out.println("Sum of matrices:");
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
